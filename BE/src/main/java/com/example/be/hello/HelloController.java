fb26b0764b516606c5acae64475d5477252b0cd4
SUBY <bsa0530@gmail.com>
2024-07-06 02:06:06 +0900

    feat: ADD hello api
    - HelloController.java
        - get: hello/{username}
    - Message(record class)

---
+++ BE/src/main/java/com/example/be/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.be.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+class HelloController {
+    @GetMapping("/{username}")
+    public Message getHello(@PathVariable String username) {
+        return new Message("안녕하세요 " + username);
+    }
+
+}
