8f92c0fe07f0ae567d59d21fb175326c363dddbb
SUBY <bsa0530@gmail.com>
2024-07-06 15:40:25 +0900

    chore: Replace New Proejct and controller

---
+++ BE/src/main/java/com/example/be/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.be.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/{username}")
+    public Message getHello(@PathVariable String username) {
+        return new Message("안녕하십니까? " + username);
+    }
+
+}
---
+++ BE/src/main/java/com/example/be/hello/Message.java
@@ -0,0 +1,4 @@
+package com.example.be.hello;
+
+public record Message(String message) {
+}
